bb2d17ea6bee4eb77d7face571860385b93bfddb
Tomasz Szyndler <tsszyndler@gmail.com>
2026-01-08 10:47:00 +0100

    encapsulation measures added

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class MainManager : MonoBehaviour
 {
-    public static MainManager Instance;
+    public static MainManager Instance { get; private set; }
 
     public Color TeamColor;
 
--- Assets/Scripts/ResourcePile.cs
+++ Assets/Scripts/ResourcePile.cs
@@ -10,7 +10,22 @@ public class ResourcePile : Building
 {
     public ResourceItem Item;
 
-    public float ProductionSpeed = 0.5f;
+    private float m_ProductionSpeed = 0.5f;
+    public float ProductionSpeed
+    {
+        get { return m_ProductionSpeed; }
+        set
+        {
+            if (value < 0.0f)
+            {
+                Debug.LogError("You can't set a negative production speed!");
+            }
+            else
+            {
+                m_ProductionSpeed = value;
+            }
+        }
+    }
 
     private float m_CurrentProduction = 0.0f;
 
@@ -23,18 +38,18 @@ public class ResourcePile : Building
 
             m_CurrentProduction = m_CurrentProduction - amountToAdd + leftOver;
         }
-        
+
         if (m_CurrentProduction < 1.0f)
         {
-            m_CurrentProduction += ProductionSpeed * Time.deltaTime;
+            m_CurrentProduction += m_ProductionSpeed * Time.deltaTime;
         }
     }
 
     public override string GetData()
     {
-        return $"Producing at the speed of {ProductionSpeed}/s";
-        
+        return $"Producing at the speed of {m_ProductionSpeed}/s";
+
     }
-    
-    
+
+
 }
